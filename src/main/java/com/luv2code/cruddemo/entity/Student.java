39cb6b74ef0f38fd35f747df0beaad2f2275c4ee
EngYoussef <youssef.alboghdady399@gmail.com>
2024-12-23 18:55:33 +0200

    firstCommit

--- src/main/java/com/luv2code/cruddemo/entity/Student.java
+++ src/main/java/com/luv2code/cruddemo/entity/Student.java
@@ -28,13 +28,54 @@ public class Student {
     public Student() {
 
     }
-    
+
     public Student(String firstName, String lastName, String email) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.email = email;
     }
 
+//    define getter/setter
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String Id) {
+        this.id = Id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+//    define toString
+    @Override
+    public String toString() {
+        return "Student [id=" + id +
+                ", firstName=" + firstName +
+                ", lastName=" + lastName +
+                ", email=" + email + "]";
+    }
 
 
 }
