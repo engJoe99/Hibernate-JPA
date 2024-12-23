88c38b8334936473b7b0a3cb52b25ae392e87a8a
Youssef Alboghdady <youssef.alboghdady399@gmail.com>
2024-12-23 18:31:53 +0200

    firstCommit

---
+++ src/main/java/com/luv2code/cruddemo/entity/Student.java
@@ -0,0 +1,40 @@
+package com.luv2code.cruddemo.entity;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name="student")
+public class Student {
+
+
+//    define fields
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email")
+    private String email;
+
+
+//    define constructors
+    public Student() {
+
+    }
+    
+    public Student(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+
+
+}
